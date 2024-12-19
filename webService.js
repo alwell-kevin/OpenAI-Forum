45b0a839f152a4a12d653a856c21761a4bac13b7
Kevin Alwell <alwellkevin@openai.com>
2024-12-19 06:55:06 -0500

    working prototype

---
+++ webService.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware to allow CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+// An endpoint which would work with the client code above - it returns
+// the contents of a REST API request to this protected endpoint
+app.get("/session", async (req, res) => {
+  const r = await fetch("https://api.openai.com/v1/realtime/sessions", {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-realtime-preview-2024-12-17",
+      voice: "verse",
+    }),
+  });
+  const data = await r.json();
+
+  // Send back the JSON we received from the OpenAI REST API
+  res.send(data);
+});
+
+app.listen(3000);
